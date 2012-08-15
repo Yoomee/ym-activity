14776625080a70c282cd2ad76b5873647f1dba4c
Matt Atkins <matt@yoomee.com>
2012-08-15 09:43:29 +0100

    Make activity items dependent destroy, add destroy_activity! method

--- lib/ym_activity/models/user_ext.rb
+++ lib/ym_activity/models/user_ext.rb
@@ -1,7 +1,11 @@
 module YmActivity::UserExt
   
   def self.included(base)
-    base.has_many :activity_items, :class_name => "YmActivity::ActivityItem"
+    base.has_many :activity_items, :class_name => "YmActivity::ActivityItem", :dependent => :destroy
+  end
+  
+  def destroy_activity!(resource)
+    activity_items.where(:resource_type => resource.class.to_s, :resource_id => resource.id).destroy_all
   end
   
   def record_activity!(resource, options = {})
